3030b55b41e4a2698924255e2bcf6edd8da4cad1
devanandana21 <tve24el018@cet.ac.in>
2026-02-13 09:27:59 +0530

    Update DivisionHandling.java

--- src/DivisionHandling.java
+++ src/DivisionHandling.java
@@ -1,5 +1,5 @@
 import java.util.scanner;
-    public class DivideNumberse{
+    public class DivisionHandling{
         public static void main(String[] args){
             Scanner sc= new Scanner(System.in);
             System.out.println("Enter first integer: ");
