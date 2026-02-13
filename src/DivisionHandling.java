13b3bb0cd6ca8fb139b2994711312c64de6fe455
devanandana21 <tve24el018@cet.ac.in>
2026-02-13 09:25:02 +0530

    Update DivisionHandling.java

--- src/DivisionHandling.java
+++ src/DivisionHandling.java
@@ -1,4 +1,4 @@
-import java.util.scanner
+import java.util.scanner;
     public class DivideNumberse{
         public static void main(String[] args){
             Scanner sc= new Scanner(System.in);
